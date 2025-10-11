2a85bcbf988460e30f8458152908d503b717df88
Trần Thị Kim Thanh <102295552+mingthanh@users.noreply.github.com>
2025-10-11 11:23:29 +0700

    update search bar
    create search bar to app

---
+++ lib/view/widgets/custom_search_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:store_app/utils/app_textstyles.dart';
+
+class CustomSearchBar extends StatelessWidget {
+  const CustomSearchBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: TextField(
+        style: AppTextStyles.withColor(
+          AppTextStyles.buttonMedium,
+          Theme.of(context).textTheme.bodyLarge?.color ?? Colors.black,
+        ),
+        decoration: InputDecoration(
+          hintText: 'Search',
+          hintStyle: AppTextStyles.withColor(
+            AppTextStyles.buttonMedium,
+            isDark ? Colors.grey[400]! : Colors.grey[600]!,
+          ),
+          prefixIcon: Icon(
+            Icons.search,
+            color: isDark ? Colors.grey[400]! : Colors.grey[600]!,
+          ),
+          filled: true,
+          fillColor: isDark ? Colors.grey[850] : Colors.grey[200],
+          contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide.none,
+          ),
+        ),
+      ),
+    );
+  }
+}
